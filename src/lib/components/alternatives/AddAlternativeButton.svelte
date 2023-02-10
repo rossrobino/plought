0db8136f26a7025f888760c1faf4212a40fed1e0
rossrobino <rbrobino@gmail.com>
2023-02-09 16:16:05 -0800

    add alternative

--- src/lib/components/alternatives/AddAlternativeButton.svelte
+++ src/lib/components/alternatives/AddAlternativeButton.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
-	import { addAlternative } from "$lib/util/alternative";
+	import { criteria, alternatives } from "$lib/stores";
+	import { get } from "svelte/store";
+
+	export const addAlternative = () => {
+		const scores: number[] = new Array($criteria.length).fill(0);
+		const pairwise: number[] = new Array($alternatives.length + 1).fill(
+			0.5,
+		);
+		const copy = [...get(alternatives)];
+		copy.forEach((alt) => {
+			alt.pairwise.push(0.5);
+		});
+		alternatives.update(() =>
+			copy.concat({
+				name: `Alternative #${get(alternatives).length + 1}`,
+				scores,
+				pairwise,
+			}),
+		);
+	};
 </script>
 
 <button class="w-full" on:click={addAlternative}>Add</button>

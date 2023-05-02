60c48128dafc7102b7e710aef1e90b45ea09e532
rossrobino <rbrobino@gmail.com>
2023-05-02 14:17:25 -0700

    add vercel analytics

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -16,19 +16,6 @@
 	<title>{headTitle}</title>
 	<meta name="description" content={desc} />
 	<meta name="keywords" content="decision, making, weighted, sum" />
-	<script
-		async
-		src="https://www.googletagmanager.com/gtag/js?id=G-10Y495MZDX"
-	></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag("js", new Date());
-
-		gtag("config", "G-10Y495MZDX");
-	</script>
 </svelte:head>
 
 <header class="-mx-4 mb-8 bg-base px-4 pb-4 text-content">

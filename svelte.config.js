3fa243a510fcfeb0ad6e8cf3d337d438e4fbf269
rossrobino <rbrobino@gmail.com>
2023-06-26 08:45:02 -0400

    svelte 4

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true,
-		}),
-	],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({ runtime: "edge" }),
 	},

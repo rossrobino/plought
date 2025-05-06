7c0344991bba8fff3ba6875ffdd6694839983687
rossrobino <rbrobino@gmail.com>
2025-05-06 17:37:17 -0400

    fix

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import type { User } from "@/lib/db/table";
 import { Layout } from "@/pages/layout";
-import googleSvg from "@/svg/google.svg";
+import googleSvg from "@/svg/google.svg?raw";
 
 export const Login = (props: { user: User | null }) => {
 	return (
@@ -8,8 +8,12 @@ export const Login = (props: { user: User | null }) => {
 			<article class="prose">
 				<h1>Login</h1>
 				<div class="border rounded-xl flex items-center justify-center p-6">
-					<a class="contents" href="/login/google">
-						<img src={googleSvg} alt="Login with Google" />
+					<a
+						class="contents"
+						href="/login/google"
+						aria-label="Login with Google"
+					>
+						{googleSvg}
 					</a>
 				</div>
 			</article>

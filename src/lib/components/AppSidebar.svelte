0d1201d147fcb40090336cefd483005b5e18e8a2
Ross Robino <ross@robino.dev>
2026-02-28 15:18:47 -0500

    github

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { page } from "$app/state";
-	import { info } from "$lib/info";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import { info } from "$lib/info";
 	import BarChart3Icon from "@lucide/svelte/icons/bar-chart-3";
 	import GitCompareIcon from "@lucide/svelte/icons/git-compare";
+	import GithubIcon from "@lucide/svelte/icons/github";
 	import HomeIcon from "@lucide/svelte/icons/home";
 	import ScaleIcon from "@lucide/svelte/icons/scale";
 
@@ -34,7 +35,11 @@
 			href="/"
 			class="flex items-center gap-2 rounded-md p-2 text-sidebar-foreground no-underline transition-colors hover:bg-transparent"
 		>
-			<img src="/plought-text-logo-dark.svg" alt={info.name} class="h-6 w-auto" />
+			<img
+				src="/plought-text-logo-dark.svg"
+				alt={info.name}
+				class="h-6 w-auto"
+			/>
 		</a>
 	</Sidebar.Header>
 	<Sidebar.Content>
@@ -68,7 +73,10 @@
 				<Sidebar.Menu>
 					{#each apps as item}
 						<Sidebar.MenuItem>
-							<Sidebar.MenuButton isActive={active(item.href)} tooltipContent={item.label}>
+							<Sidebar.MenuButton
+								isActive={active(item.href)}
+								tooltipContent={item.label}
+							>
 								{#snippet child({ props })}
 									<a {...props} href={item.href}>
 										<item.icon />
@@ -88,7 +96,10 @@
 				<Sidebar.Menu>
 					{#each summary as item}
 						<Sidebar.MenuItem>
-							<Sidebar.MenuButton isActive={active(item.href)} tooltipContent={item.label}>
+							<Sidebar.MenuButton
+								isActive={active(item.href)}
+								tooltipContent={item.label}
+							>
 								{#snippet child({ props })}
 									<a {...props} href={item.href}>
 										<item.icon />
@@ -102,4 +113,24 @@
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 	</Sidebar.Content>
+	<Sidebar.Footer>
+		<Sidebar.Separator />
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton tooltipContent="GitHub Repository">
+					{#snippet child({ props })}
+						<a
+							{...props}
+							href="https://github.com/rossrobino/plought"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<GithubIcon />
+							<span>GitHub</span>
+						</a>
+					{/snippet}
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Footer>
 </Sidebar.Root>

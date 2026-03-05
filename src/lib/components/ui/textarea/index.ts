45987a488fe1058ed2464713f2bfc0294b16c172
Ross Robino <ross@robino.dev>
2026-03-05 10:19:27 -0500

    Rename components to kebab-case

--- src/lib/components/ui/textarea/index.ts
+++ src/lib/components/ui/textarea/index.ts
@@ -1,7 +1,10 @@
+import AutosizeRoot from "./autosize-textarea.svelte";
 import Root from "./textarea.svelte";
 
 export {
+	AutosizeRoot,
 	Root,
 	//
+	AutosizeRoot as AutosizeTextarea,
 	Root as Textarea,
 };
